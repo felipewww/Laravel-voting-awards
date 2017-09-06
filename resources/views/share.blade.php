3fe98063b34b115320918c195d7a4f06752770c4
felipewww <felipewww@outlook.com.br>
2017-09-06 19:48:23 -0300

    share..

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -2,7 +2,7 @@
 <head>
     <title>Startup Awards {{ env('APP_YEAR') }}</title>
     <meta property="fb:app_id"        content="{{ \App\Http\Controllers\Controller::$FB_APP_ID }}" />
-    <meta property="og:url"           content="{{ env('APP_URL') }}/{{$catid}}/{{$name}}" />
+    <meta property="og:url"           content="{{ env('APP_URL') }}/{{$cat_id}}/{{$name}}" />
     <meta property="og:type"          content="website" />
     <meta property="og:title"         content="Startup Awards {{env("APP_YEAR")}}" />
     <meta property="og:description"   content="Indiquei {{ $name }} para a Startup Awards {{env("APP_YEAR")}} na categoria {{ $cat_name }}, indique você também!" />
